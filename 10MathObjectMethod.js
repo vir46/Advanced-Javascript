05bbc078da361b2caef7aab190f2fc0e099d858b
Virtus Tan <virtustanofficial@gmail.com>
2021-12-02 18:23:02 +0700

    Advanced Javascript

---
+++ 10MathObjectMethod.js
@@ -0,0 +1,35 @@
+Math.abs(x) // Returns the absolute value of x.
+Math.acos(x) //Returns the arccosine of x.
+Math.acosh(x) // Returns the hyperbolic arccosine of x.
+Math.asin(x) // Returns the arcsine of x.
+Math.asinh(x) //Returns the hyperbolic arcsine of a number.
+Math.atan(x) //Returns the arctangent of x.
+Math.atanh(x) //Returns the hyperbolic arctangent of x.
+Math.atan2(y, x) //Returns the arctangent of the quotient of its arguments.
+Math.cbrt(x) //Returns the cube root of x.
+Math.ceil(x) //Returns the smallest integer greater than or equal to x.
+Math.clz32(x) //Returns the number of leading zero bits of the 32-bit integer x.
+Math.cos(x) //Returns the cosine of x.
+Math.cosh(x) //Returns the hyperbolic cosine of x.
+Math.exp(x) //Returns e^x, where x is the argument, and e is Euler's constant (2.718…, the base of the natural logarithm).
+Math.expm1(x) //Returns subtracting 1 from exp(x).
+Math.floor(x) //Returns the largest integer less than or equal to x.
+Math.fround(x) //Returns the nearest single precision float representation of x.
+Math.hypot([x[1, y[1,2]]]) //Returns the square root of the sum of squares of its arguments.
+Math.imul(x, y) //Returns the result of the 32-bit integer multiplication of x and y.
+Math.log(x) //Returns the natural logarithm (㏒e; also, ㏑) of x.
+Math.log1p(x) //Returns the natural logarithm (㏒e; also ㏑) of 1 + x for the number x.
+Math.log10(x) //Returns the base-10 logarithm of x.
+Math.log2(x) //Returns the base-2 logarithm of x.
+Math.max([x[1, y[1, 2]]]) //Returns the largest of zero or more numbers.
+Math.min([x[1, y[1, 2]]]) //Returns the smallest of zero or more numbers.
+Math.pow(x, y) //Returns base x to the exponent power y (that is, x^y).
+Math.random() //Returns a pseudo-random number between 0 and 1.
+Math.round(x) //Returns the value of the number x rounded to the nearest integer.
+Math.sign(x) //Returns the sign of the x, indicating whether x is positive, negative, or zero.
+Math.sin(x) //Returns the sine of x.
+Math.sinh(x) //Returns the hyperbolic sine of x.
+Math.sqrt(x) //Returns the positive square root of x.
+Math.tan(x) //Returns the tangent of x.
+Math.tanh(x) //Returns the hyperbolic tangent of x.
+Math.trunc(x) //Returns the integer portion of x, removing any fractional digits.
